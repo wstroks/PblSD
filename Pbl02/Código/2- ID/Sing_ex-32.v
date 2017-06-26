cce7527ab930a69260558adaa44300b925753839
Washington Pagotto Batista <wstroks@gmail.com>
2017-06-26 09:42:32 -0300

    comparador e extend26

--- Pbl02/Código/2- ID/Sing_ex-32.v
+++ Pbl02/Código/2- ID/Sing_ex-32.v
@@ -6,8 +6,19 @@ module SignExtend(
 );
 
 	always @(*) begin
-		if(controle==1)begin
-      			saida[31:0] <= {{16{1'b0}},imediato[15:0]};
+			if(controle==1)begin
+
+			if(imediato[15]==1'b0)begin
+				saida[31:0] <= {{16'b0},imediato[15:0]};
+			end
+			else begin
+				saida[31:0] <= {{16'b1111111111111111},imediato[15:0]};
+			end
+
+		end
+
+		else begin
+			saida[31:0] <= {{16{1'b0}},imediato[15:0]};
 		end
 	end
 endmodule 
